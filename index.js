e8f30777fc35e577f4f53f99f9e5512cfbd15f07
Evan Furniss <evanfurniss@gmail.com>
2021-01-28 15:56:47 -0600

    created the other classes, required them on my index.js

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
+const employee = require("./util/constructors")
+const manager = require("./util/constructors")
+const engineer = require("./util/constructors")
+const intern = require("./util/constructors")
 
+
+inquirer
+  .prompt() [
+    {
+      type: "input",
+      message: "Enter your full name",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "Enter your employee ID",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "Enter your email address",
+      name: "email"
+    }
+  ]
--- util/constructors.js
+++ util/constructors.js
@@ -2,6 +2,7 @@ class Employee{
     constructor (name, email, id) {
         this.name = name;
         this.email = email;
+        this.title = "Employee"
         this.id = id;
     }
 }
@@ -10,6 +11,23 @@ class Manager extends Employee {
     constructor (name, email, id){
         super(name, email, id);
         this.officeNum = officeNum;
+        this.title = "Manager";
+    }
+}
+
+class Engineer extends Employee {
+    constructor(name, email, id, git) {
+        super(name, email, id);
+        this.github = github;
+        this.title = "Engineer";
+    }
+}
+
+class Intern extends Employee {
+    constructor (name, email, id, school) {
+        super(name, email, id);
+        this.school = school;
+        this.title = "Intern";
     }
 }
 
